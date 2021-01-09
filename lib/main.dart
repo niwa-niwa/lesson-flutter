662ceccfa11cb57c8441db7295b5f652ceb23d2c
niwa-niwa <niwayama1101@gmail.com>
2021-01-09 17:46:26 +0900

    added button and action put a messege in debug-console after press the button

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,31 @@ void main() {
   ));
 }
 
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('HelloHello, world'),
+            ),
+          ),
+          MyButton()
+        ],
+      ),
+    );
+  }
+}
+
+
 
 class MyAppBar extends StatelessWidget{
   MyAppBar({this.title});
@@ -44,25 +69,27 @@ class MyAppBar extends StatelessWidget{
 }
 
 
-class MyScaffold extends StatelessWidget {
+class MyButton extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text('HelloHello, world'),
-            ),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was Tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal:8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: Center(
+          child: Text('Engage'),
+        ),
       ),
     );
   }
 }
+
+
