a4a97dd469c7fdc82fab917d4eef23a2b1052915
niwa-niwa <niwayama1101@gmail.com>
2021-01-10 11:50:34 +0900

    complated todo-app

--- lib/main.dart
+++ lib/main.dart
@@ -20,19 +20,7 @@ class MyApp extends StatelessWidget {
 
       //using theme
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 
@@ -44,41 +32,124 @@ class MyApp extends StatelessWidget {
 
 
 // the widget display todo list
-class TodoListPage extends StatelessWidget{
+class TodoListPage extends StatefulWidget{
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+// the state is for TodoListPage
+class _TodoListPageState extends State<TodoListPage>{
+
+  // variable that manage state
+  final List<String> todoList = [];
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: Text('リスト一覧画面')
+
+      // title of this page
+      appBar: AppBar(
+        title: Text('Todo List')
       ),
 
+      // main display
+      body: ListView.builder(
+
+        // pass number of item to itemBuilder
+        itemCount: todoList.length,
+
+        //like for statement
+        itemBuilder: (context, index){
+          return Card(
+            child: ListTile(
+              title: Text(todoList[index]),
+            )
+          );
+        },
+      ),
+
+      // made a float button
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.of(context).push(
+        onPressed: () async {
+          // move to TodoAddPage and waiting a variable new task
+          final newListText = await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
               return TodoAddPage();
             }),
           );
+          // sets state of todolist. 
+          //if pressed the cancel button in TodoAddPage the page returned null
+          if (newListText != null){
+            setState((){
+              todoList.add(newListText);
+            });
+          }
         },
         child: Icon(Icons.add),
       ),
+
     );
   }
 }
 
-class TodoAddPage extends StatelessWidget{
+
+// the widget display add todo
+class TodoAddPage extends StatefulWidget{
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
+class _TodoAddPageState extends State<TodoAddPage> {
+
+  // the variable state
+  String _text = '';
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          onPressed: (){
-            Navigator.of(context).pop();
-          },
-          child: Text('リスト追加画面画面(タップで戻る)'),
-        ),
+      
+      appBar: AppBar(
+        title: Text('Add Todo'),
       ),
+
+      body: Container(
+
+        padding: EdgeInsets.all(64),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_text, style: TextStyle(color: Colors.blue)),
+            TextField(
+              onChanged: (String value){
+                setState((){
+                  _text = value;
+                });
+              },
+            ),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.blue,
+                onPressed: (){
+                  Navigator.of(context).pop(_text);
+                },
+                child: Text('Add list', style:TextStyle(color: Colors.white)),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: FlatButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel')
+              ),
+            ),
+          ],
+        )
+
+      )
     );
   }
 }
