584546dff7b881c178d4ffe4a26dce1a175f1ffb
niwa-niwa <niwayama1101@gmail.com>
2021-01-11 18:24:37 +0900

    be able to transition to other pages to tap icons

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -16,58 +17,106 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: 
-        Scaffold(
-          appBar:
-            PreferredSize(
-              preferredSize: Size.fromHeight(0.0),
-              child: AppBar(
-                  backgroundColor: Colors.black87,
-              )
-            ),
-          drawer:
-            Drawer(
-              child: ListView(
-                children: <Widget>[
-                  DrawerHeader(
-                    child: Text("Categories"),
-                  ),
-                  ListTile(
-                    leading:Icon(Icons.restaurant),
-                    title: Text('Resturant'),
-                  ),
-                ],
-              ),
-            ),
-          body:
-            WebView(
-              initialUrl: 'https://ja.wikipedia.org/wiki/Flutter',
-              javascriptMode: JavascriptMode.unrestricted,
-            ),
-          bottomNavigationBar:
-            BottomNavigationBar(
-              backgroundColor: Colors.blue[100],
-              items:[
-                BottomNavigationBarItem(
-                  label: 'MENU',
-                  icon: IconButton(
-                    icon: Icon(Icons.menu),
-                    onPressed: (){
-                    },
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.favorite),
-                  label: 'LIKE',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.bookmark),
-                  label: 'Bookmark',
-                ),
-              ],
-            ),
-        ),
+      home: MainWebView(),
     );
   }
 }
 
+
+class MainWebView extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _MainWebViewState();
+}
+
+class _MainWebViewState extends State<MainWebView>{
+
+  String homeUrl = 'https://ja.wikipedia.org/wiki/Flutter';
+  WebViewController _controller;
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      key: _key,
+      appBar:
+        PreferredSize(
+          preferredSize: Size.fromHeight(0.0),
+          child: AppBar(
+              backgroundColor: Colors.black87,
+          )
+        ),
+
+      drawer:
+        Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: Text("Categories"),
+              ),
+              ListTile(
+                leading:Icon(Icons.home),
+                title: Text('Home'),
+                onTap: (){
+                  _controller.loadUrl(homeUrl);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading:Icon(Icons.restaurant),
+                title: Text('Resturants'),
+              ),
+              ListTile(
+                leading:Icon(Icons.museum),
+                title: Text('Museums'),
+              ),
+              ListTile(
+                leading:Icon(Icons.business),
+                title: Text('Facilities'),
+              ),
+              ListTile(
+                leading:Icon(Icons.train),
+                title: Text('Access'),
+              ),
+
+            ],
+          ),
+        ),
+
+      body:
+        WebView(
+          initialUrl: homeUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController controller){
+            _controller = controller;
+          },
+        ),
+
+      bottomNavigationBar:
+        BottomNavigationBar(
+          items:[
+            BottomNavigationBarItem(
+              label: 'MENU',
+              icon: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () => _key.currentState.openDrawer(),
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'LIKE',
+              icon: IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {},
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Bookmark',
+              icon: IconButton(
+                icon: Icon(Icons.bookmark),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+}
